39d6755e8f0e8beb8db86d5c0fc2a1159d1d67e2
naderjlyr <naderjlyr@gmail.com>
2022-05-27 16:41:04 +0300

    removing attribute

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -29,10 +29,7 @@ const LeafletMap = ({ location, job, position }: IProps) => {
       scrollWheelZoom={false}
       style={{ width: "100%", height: "100%" }}
     >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://api.mapbox.com/styles/v1/naderjlyr/cl3m5g2a1005s15od3r2nfrr5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibmFkZXJqbHlyIiwiYSI6ImNsM200YjhkMTAxODMzamxuOWJybTk5OXgifQ.Ros4f-cCXwiakGLffDXibQ"
-      />
+      <TileLayer url="https://api.mapbox.com/styles/v1/naderjlyr/cl3m5g2a1005s15od3r2nfrr5/tiles/256/{z}/{x}/{y}@2x?access_token=pk.eyJ1IjoibmFkZXJqbHlyIiwiYSI6ImNsM200YjhkMTAxODMzamxuOWJybTk5OXgifQ.Ros4f-cCXwiakGLffDXibQ" />
       {position && (
         <Marker icon={pinIcon} position={position}>
           <Popup closeButton={true}>
